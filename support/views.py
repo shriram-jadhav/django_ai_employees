73db88db0fc8f6ae333b3c63f176701fbfe09c4a
Shriram Jadhav <shriram26102004@gmail.com>
2026-08-12 06:43:43 +0530

    added response header to streaming response

--- support/views.py
+++ support/views.py
@@ -74,4 +74,7 @@ def conversation_stream(request, conversation_id):
                 yield f"data: {json.dumps(event)}\n\n"
         finally:
             unsubscribe(conversation_id, q)
-    return StreamingHttpResponse(event_stream(conversation_id), content_type="text/event-stream")
+    response  = StreamingHttpResponse(event_stream(conversation_id), content_type="text/event-stream")
+    response['Cache-Control'] = 'no-cache'
+    response['X-AccelBuffering'] = 'no'
+    return response
